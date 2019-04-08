62cac7a314bf5ae1213cbf3349690a8be9f71829
Ryan Walker <rytwalker@gmail.com>
2019-04-07 23:02:18 -0400

    added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const graphqlHttp = require('express-graphql');
+const cors = require('cors');
 const graphqlSchema = require('./graphql/schema/index');
 const resolvers = require('./graphql/resolvers/index');
 const isAuth = require('./middleware/is-auth');
@@ -7,6 +8,16 @@ const isAuth = require('./middleware/is-auth');
 const app = express();
 
 app.use(express.json());
+// app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(isAuth);
 
 app.use(
